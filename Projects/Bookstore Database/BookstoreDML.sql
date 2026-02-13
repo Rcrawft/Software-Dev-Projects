d384c17a84b0376a0dc2915ce63720d6478fb9cc
Rcrawft <crawfordryan275@gmail.com>
2026-02-12 22:22:01 -0600

    Create BookstoreDML.sql

---
+++ Projects/Bookstore Database/BookstoreDML.sql
@@ -0,0 +1,128 @@
+#Ryan Crawford
+#This file contains the DML for the bookstore database.
+  
+#inserting books into book table
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (1, 'A Game of Thrones', 'George R.R. Martin', 'Fantasy', '1996-8-1', 'Bantam Spectra', 'Bantam First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (2, 'A Clash of Kings', 'George R.R. Martin', 'Fantasy', '1998-11-16', 'Batnam Spectra', 'Bantam First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (3, 'A Storm of Swords', 'George R.R. Martin', 'Fantasy', '2000-8-8', 'Batnam Spectra', 'Bantam First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (4, 'A Feast for Crows', 'George R.R. Martin', 'Fantasy', '2005-10-17', 'Batnam Spectra', 'Bantam First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (5, 'A Dance with Dragons', 'George R.R. Martin', 'Fantasy', '2011-7-12', 'Batnam Spectra', 'Bantam First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (6, 'A Game of Thrones', 'George R.R. Martin', 'Fantasy', '2010-12-23', 'Harper Voyager', 'Harper Voyager First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (7, 'Mistborn: The Final Empire', 'Brandon Sanderson', 'Fantasy', '2006-7-17', 'Tor Books', 'Tor First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (8, 'Mistborn: The Well of Ascension', 'Brandon Sanderson', 'Fantasy', '2007-8-21', 'Tor Books', 'Tor First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (9, 'Mistborn: The Hero of Ages', 'Brandon Sanderson', 'Fantasy', '2008-10-14', 'Tor Books', 'Tor First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (10, 'Mistborn: The Final Empire', 'Brandon Sanderson', 'Fantasy', '2006-7-17', 'Tor Books', 'Tor First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (11, 'Mistborn', 'Brandon Sanderson', 'Fantasy', '2007-7-31', 'Tor Books', 'Tor First, First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (12, 'The Blade Itself', 'Joe Abercrombie', 'Fantasy', '2006-5-4', 'Gollancz', 'Gollancz First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (13, 'Before They Are Hanged', 'Joe Abercrombie', 'Fantasy', '2007-3-15', 'Gollancz', 'Gollancz First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (14, 'Last Argument of Kings', 'Joe Abercrombie', 'Fantasy', '2008-3-20', 'Gollancz', 'Gollancz First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (15, 'Last Argument of Kings', 'Joe Abercrombie', 'Fantasy', '2018-9-20', 'Gollancz', 'Gollancz 10 Anniversary Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (16, 'The Fellowship of the Ring', 'J.R.R. Tolkien', 'Fantasy', '1954-7-29', 'Houghton Mifflin', 'First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (17, 'The Two Towers', 'J.R.R. Tolkien', 'Fantasy', '1954-9-11', 'Houghton Mifflin', 'First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (18, 'The Return of the King', 'J.R.R. Tolkien', 'Fantasy', '1955-10-20', 'Houghton Mifflin', 'First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (19, 'The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 'Science Fiction', '1979-10-12', 'Arthur Barker Limited', 'First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (20, 'Dune', 'Frank Herbert', 'Science Fiction', '1965-8-1', 'Chilton Books', 'First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (21, 'The Stand', 'Stephen King', 'Post-Apocalyptic', '1990-5-1', 'Doubleday', 'Complete and Uncut Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (22, 'Pet Sematary', 'Stephen King', 'Horror', '1983-11-4', 'Doubleday', 'First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (23, 'It', 'Stephen King', 'Horror', '1986-9-15', 'Doubledat', 'First Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (24, 'Dracula', 'Bram Stoker', 'Horror', '1986-5-12', 'Norton', 'Norton Critical Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (25, 'The Lord of the Rings', 'J.R.R. Tolkien', 'Fantasy', '2005-10-12', 'Houghton Mifflin Harcourt', '50th Anniversary One-Volume Edition');
+insert into book_t (book_id, book_title, author_name, genre, publish_date, publisher, edition)
+values (26, 'Frankenstein', 'Mary Shelley', 'Horror', '2013-10-3', 'Penguin Classics', 'Penguin Clothbound Classics Edition');
+
+#inserting employees into employees table
+insert into employee_t (employee_id, employee_name, employee_address, employee_email)
+values (1, 'James Odjewuyi', '123 Elm Street', 'jo@email.com');
+insert into employee_t (employee_id, employee_name, employee_address, employee_email)
+values (2, 'Ryan Crawford', '456 Carleon Street', 'rc@email.com');
+
+#inserting customers into customers table
+insert into customer_t (customer_id, customer_name, customer_address, customer_city, customer_state, customer_postalcode, customer_email)
+values (1, 'Valen Dreth', 'Imperial Prison, Cell 2', 'Imperial City', 'CY', '12345-6789', 'prisonelf@cyromail.com');
+insert into customer_t (customer_id, customer_name, customer_address, customer_city, customer_state, customer_postalcode, customer_email)
+values (2, 'Elend Venture', '118 6th Street, Keep Venture', 'Luthadel', 'FE', '73497-1138', 'kinglyscholar@luthamail.com');
+insert into customer_t (customer_id, customer_name, customer_address, customer_city, customer_state, customer_postalcode, customer_email)
+values (3, 'Vin Venture', '118 6th Street, Keep Venture', 'Luthadel', 'FE', '73497-1138', 'mistassassin@luthamail.com');
+insert into customer_t (customer_id, customer_name, customer_address, customer_city, customer_state, customer_postalcode, customer_email)
+values (4, 'Brynden Rivers', '1837 Spooky Tree', 'Haunted Forest', 'WS', '00001-1000', 'thousandeyes@treemail.com');
+insert into customer_t (customer_id, customer_name, customer_address, customer_city, customer_state, customer_postalcode, customer_email)
+values (5, 'Shy South', '23 1st Street', 'Squaredeal', 'SL', '12837-0509', 'gunsmoke@farmail.com');
+
+#inserting orders into orders table
+insert into order_t (order_id, employee_id, customer_id)
+values (1032, 1, 2);
+insert into order_t (order_id, employee_id, customer_id)
+values (1033, 1, 3);
+insert into order_t (order_id, employee_id, customer_id)
+values (1034, 2, 5);
+insert into order_t (order_id, employee_id, customer_id)
+values(1035, 2, 4);
+insert into order_t (order_id, employee_id, customer_id)
+values (1036, 1, 1);
+insert into order_t (order_id, employee_id, customer_id)
+values (1037, 2, 2);
+insert into order_t (order_id, employee_id, customer_id)
+values (1038, 1, 4);
+
+#inserting placed orders into placed_orders table
+insert into placed_order_t (order_id, book_id, order_date)
+values (1032, 1, '2025-2-18');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1033, 12, '2025-2-18');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1034, 21, '2025-2-20');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1035, 24, '2025-3-2');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1036, 11, '2025-3-3');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1037, 2, '2025-3-10');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1037, 3, '2025-3-10');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1037, 4, '2025-3-10');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1037, 5, '2025-3-10');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1038, 26, '2025-3-15');
+insert into placed_order_t (order_id, book_id, order_date)
+values (1038, 20, '2025-3-15');
+
+#describes each table
+describe customer_t;
+describe employee_t;
+describe book_t;
+describe order_t;
+describe placed_order_t;
+
+#shows each table along with their contents
+select * from customer_t;
+select * from employee_t;
+select * from book_t;
+select * from order_t;
+select * from placed_order_t;
